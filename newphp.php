b9aa77a3bff995f560e356c5d882443db3f7d8ef
Marinatok <marinatokareva1505@gmail.com>
2022-03-10 16:44:27 +0200

    create files for hw

---
+++ newphp.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>корзина с товарами</title>
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+    <header>
+        <?php
+            include 'header.php';
+        ?>
+    </header>
+    <body>
+        <hr>
+        <div class="card-group">
+            <div class="card" style="width: 18rem;">
+                <img src="./images/3b734f7fb1034f57929299cad0ac1909.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Название карточки</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Переход куда-нибудь</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="./images/images.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Название карточки</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Переход куда-нибудь</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="./images/zhinocha-vishivanka-ruchnoi-roboti-frantsuzki-kviti-fioletovi3622-51850168353458.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Название карточки</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Переход куда-нибудь</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="./images/b11773.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Название карточки</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Переход куда-нибудь</a>
+                </div>
+            </div>
+        </div>
+        <hr>
+    </body>
+    <footer>
+        <?php
+        include 'footer.php';
+        ?>
+    </footer>
+</html>
